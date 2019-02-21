84328499a027d02b3ff516558b78181e0b4640da
Kevin Weiss <kevin.weiss@emory.edu>
2019-02-21 12:35:52 -0500

    Edit EPT figure

--- analysis/Figure 2.R
+++ analysis/Figure 2.R
@@ -14,7 +14,7 @@ library("gridExtra")
 # All partners
 # Main partners: 8023-8032, cas cov:8039:8048, inst cov: 8055:8064
 
-tiff(filename = "analysis/Fig 2.tiff", height = 8, width = 11, units = "in", res = 250)
+tiff(filename = "analysis/Figure 2.tiff", height = 8, width = 11, units = "in", res = 250)
 
 par(mfrow = c(2, 2), mar = c(3, 3, 2, 1.2), oma = c(0, 0, 0, 0), mgp = c(2,1,0))
 sims <- c(8000, 8002, 8004, 8006, 8008, 8010)
@@ -22,11 +22,11 @@ pal <- viridis::viridis(n = length(sims), option = "D")
 for (i in seq_along(sims)) {
   fn <- list.files("data", pattern = as.character(sims[i]), full.names = TRUE)
   load(fn)
-  sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
+  #sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
        main = "All Partners",
-       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 6))
+       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 11))
 }
 legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
@@ -35,11 +35,11 @@ pal <- viridis::viridis(n = length(sims), option = "D")
 for (i in seq_along(sims)) {
   fn <- list.files("data", pattern = as.character(sims[i]), full.names = TRUE)
   load(fn)
-  sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
+  #sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
        main = "Main Partners",
-       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 6))
+       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 11))
 }
 legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
@@ -49,11 +49,11 @@ pal <- viridis::viridis(n = length(sims), option = "D")
 for (i in seq_along(sims)) {
   fn <- list.files("data", pattern = as.character(sims[i]), full.names = TRUE)
   load(fn)
-  sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
+  #sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
        main = "Casual Partners",
-       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 6))
+       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 11))
 }
 legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
@@ -63,11 +63,11 @@ pal <- viridis::viridis(n = length(sims), option = "D")
 for (i in seq_along(sims)) {
   fn <- list.files("data", pattern = as.character(sims[i]), full.names = TRUE)
   load(fn)
-  sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
+  #sim <- mutate_epi(sim, ir100.gcct = sum(ir100.ct, ir100.gc))
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
        main = "One-Time Partners",
-       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 6))
+       xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 11))
 }
 legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
