f594a2f953cd29cea4ab0b8ff5202c464e55bf88
Kevin Weiss <kevin.weiss@emory.edu>
2019-02-27 15:08:03 -0500

    Add more detail to EPT figure

--- analysis/Figure 2.R
+++ analysis/Figure 2.R
@@ -16,7 +16,7 @@ library("gridExtra")
 
 tiff(filename = "analysis/Figure 2.tiff", height = 8, width = 11, units = "in", res = 250)
 
-par(mfrow = c(2, 2), mar = c(3, 3, 2, 1.2), oma = c(0, 0, 0, 0), mgp = c(2,1,0))
+par(mfrow = c(2, 2), mar = c(3, 3, 2, 1.2), oma = c(0, 0, 2, 0), mgp = c(2,1,0))
 sims <- c(8000, 8002, 8004, 8006, 8008, 8010)
 pal <- viridis::viridis(n = length(sims), option = "D")
 for (i in seq_along(sims)) {
@@ -25,10 +25,11 @@ for (i in seq_along(sims)) {
   #sim <- mutate_epi(sim, ir100.gcct = ir100.ct + ir100.gc)
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
-       main = "All Partners",
+       main = "Provision to All Partners",
        xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 12))
 }
-legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
+legend("bottomleft", legend = c("0% coverage", "20% coverage", "40% coverage",
+                                "60% coverage" , "80% coverage", "100% coverage"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
 sims <- c(8000, 8024, 8026, 8028, 8030, 8032)
 pal <- viridis::viridis(n = length(sims), option = "D")
@@ -38,10 +39,11 @@ for (i in seq_along(sims)) {
   #sim <- mutate_epi(sim, ir100.gcct = ir100.ct + ir100.gc)
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
-       main = "Main Partners",
+       main = "Provision to Main Partners",
        xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 12))
 }
-legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
+legend("bottomleft", legend = c("0% coverage", "20% coverage", "40% coverage",
+                                "60% coverage" , "80% coverage", "100% coverage"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
 
 sims <- c(8000, 8040, 8042, 8044, 8046, 8048)
@@ -52,10 +54,11 @@ for (i in seq_along(sims)) {
   #sim <- mutate_epi(sim, ir100.gcct = ir100.ct + ir100.gc)
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
-       main = "Casual Partners",
+       main = "Provision to Casual Partners",
        xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 12))
 }
-legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
+legend("bottomleft", legend = c("0% coverage", "20% coverage", "40% coverage",
+                                "60% coverage" , "80% coverage", "100% coverage"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
 
 sims <- c(8000, 8055:8064)
@@ -66,10 +69,13 @@ for (i in seq_along(sims)) {
   #sim <- mutate_epi(sim, ir100.gcct = ir100.ct + ir100.gc)
   plot(sim, y = "ir100.gcct", add = i > 1,
        mean.col = pal[i], qnts.col = pal[i], qnts.alpha = 0.3, qnts = 0.0,
-       main = "One-Time Partners",
+       main = "Provision to One-Time Partners",
        xlab = "Week", ylab = "IR per 100 PYAR", ylim = c(0, 12))
 }
-legend("bottomleft", legend = c("0%", "20%", "40%", "60%", "80%", "100%"),
+legend("bottomleft", legend = c("0% coverage", "20% coverage", "40% coverage",
+                                "60% coverage" , "80% coverage", "100% coverage"),
        col = pal, lwd = 3, cex = 0.85, bty = "n")
+mtext("Incidence Rates by EPT Coverage with Provision Limited by Type of Partnership",
+      outer = TRUE, cex = 1.5)
 
 dev.off()
